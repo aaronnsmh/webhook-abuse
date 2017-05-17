a2486bd4e33f81824378d01897ff053b2b4cee84
oliver <oliver@nigge.rs>
2017-05-17 20:43:48 +0200

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,12 +1,9 @@
 var request = require('request');
-var headers = {
-  "Content-Type": "application/json"
-}
 
 var options = {
   url: "WEBHOOK_URL",
   method:  "POST",
-  headers: headers,
+  headers: "Content-Type": "application/json",
   form: {"content": "@everyone oliver was here",
          "username": "Akeno",
          "avatar_url": "http://i.imgur.com/vYpVvfH.jpg"}
