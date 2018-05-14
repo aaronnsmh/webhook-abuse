324426cb3d7c9241521cf0be8cfa8cd366e2b572
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-14 21:28:05 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -12,7 +12,7 @@ var options = {
          "avatar_url": "https://i.ytimg.com/vi/QBgvHkKLPdA/hqdefault.jpg"}
 }
 
-var interval = 10 * 1; // setting interval at .35 seconds so we dont get ratelimited
+var interval = 10 * 10; // setting interval at .35 seconds so we dont get ratelimited
 for (var x = 0; x < 10000; x+=1) { // change '100' to howmany messages u wanna send
   setTimeout( function (x) {
     request(options, function(error, body){
