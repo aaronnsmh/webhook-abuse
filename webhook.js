ce5f89ca0a935be6f31a76f01f9f5184ea4d3d5f
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-17 18:55:33 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -4,12 +4,39 @@ var headers = {
 }
 
 var options = {
-  url: "https://discordapp.com/api/webhooks/445628373407105037/mrp3krDu4avfXtCqGD9mUf240Kd-TJJYcez_IVLWGcoPZUplTeQq-Q8VuEmSxvzhU6ji",
+  url: "https://discordapp.com/api/webhooks/446732308297285652/-2aGkeJJrzFs3WyF8RSIXwrmDqEVfmxlaBbMOm6WP9AOQW5yJgCTgCLUNqrJcUX_hpfp",
   method:  "POST",
   headers: headers,
-  form: {"content": "@everyone chat is movin so fast no one will see im gey.",
+  form: {"content": "@everyone hi.",
          "username": "my name is jeff",
-         "avatar_url": "https://i.ytimg.com/vi/QBgvHkKLPdA/hqdefault.jpg"}
+         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
+}
+
+var options2 = {
+  url: "https://discordapp.com/api/webhooks/446732369378934787/JWF99E0drdBsEBgtrhDpr5rEaNlc4w4XTG50uUk1H8TEBjG44p3jHNN5A4wbEPFE1mYs",
+  method:  "POST",
+  headers: headers,
+  form: {"content": "@everyone hi.",
+         "username": "my name is jeff",
+         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
+}
+
+var options3 = {
+  url: "https://discordapp.com/api/webhooks/446732420775936001/YGzn2Fu-fmi9Y0-yWNrfb-2RZqvyIqpz9GRyRQqVoJwJRINQ8vyzeQFkQocl9gocW7nj",
+  method:  "POST",
+  headers: headers,
+  form: {"content": "@everyone hi.",
+         "username": "my name is jeff",
+         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
+}
+
+var options4 = {
+  url: "https://discordapp.com/api/webhooks/446732479077023744/GElG7Agz8z82rtn_E1tv5uc9trjJhgFy2sMUC2-ltOQoRtV7iJBas_TZJ56t9eepJI5p",
+  method:  "POST",
+  headers: headers,
+  form: {"content": "@everyone hi.",
+         "username": "my name is jeff",
+         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
 }
 
 var interval = 10 * 10; // setting interval at .35 seconds so we dont get ratelimited
@@ -18,5 +45,15 @@ for (var x = 0; x < 10000; x+=1) { // change '100' to howmany messages u wanna s
     request(options, function(error, body){
       if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
     })
+    request(options2, function(error, body){
+      if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
+    })
+    request(options3, function(error, body){
+      if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
+    })
+    request(options4, function(error, body){
+      if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
+    })
   }, interval * x, x);
 }
+
