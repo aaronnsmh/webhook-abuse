c353411e39cad1d7e56881186eb8e0edf1e75214
Aaron <35376325+aaronnsmh@users.noreply.github.com>
2018-05-30 11:53:11 +0100

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -4,40 +4,15 @@ var headers = {
 }
 
 var options = {
-  url: "https://discordapp.com/api/webhooks/446732308297285652/-2aGkeJJrzFs3WyF8RSIXwrmDqEVfmxlaBbMOm6WP9AOQW5yJgCTgCLUNqrJcUX_hpfp",
+  url: "https://discordapp.com/api/webhooks/451336879296741387/2KQ4WgxJl0_HdhDzb4a4MSDA0b9hcLJ6fY0Uc2iKBifsmNC4oSy_su_hKyM-XtVNDehx",
   method:  "POST",
   headers: headers,
-  form: {"content": "@everyone hi.",
-         "username": "my name is jeff",
-         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
+  form: {"content": "@TheReal#1781.",
+         "username": "iBot",
+         "avatar_url": "https://images-ext-2.discordapp.net/external/Q-vxhw7onm06dGerOPEMT6n_RiYQFfPIiH5imJg875g/https/cdn.discordapp.com/icons/444159749458755594/bf3e28de726e8521e6e3e25830b0c0a7.jpg?width=80&height=80"}
 }
 
-var options2 = {
-  url: "https://discordapp.com/api/webhooks/446732369378934787/JWF99E0drdBsEBgtrhDpr5rEaNlc4w4XTG50uUk1H8TEBjG44p3jHNN5A4wbEPFE1mYs",
-  method:  "POST",
-  headers: headers,
-  form: {"content": "@everyone hi.",
-         "username": "my name is jeff",
-         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
-}
 
-var options3 = {
-  url: "https://discordapp.com/api/webhooks/446732420775936001/YGzn2Fu-fmi9Y0-yWNrfb-2RZqvyIqpz9GRyRQqVoJwJRINQ8vyzeQFkQocl9gocW7nj",
-  method:  "POST",
-  headers: headers,
-  form: {"content": "@everyone hi.",
-         "username": "my name is jeff",
-         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
-}
-
-var options4 = {
-  url: "https://discordapp.com/api/webhooks/446732479077023744/GElG7Agz8z82rtn_E1tv5uc9trjJhgFy2sMUC2-ltOQoRtV7iJBas_TZJ56t9eepJI5p",
-  method:  "POST",
-  headers: headers,
-  form: {"content": "@everyone hi.",
-         "username": "my name is jeff",
-         "avatar_url": "https://i.ytimg.com/vi/JilHZ_DdBYg/maxresdefault.jpg"}
-}
 
 var interval = 10 * 10; // setting interval at .35 seconds so we dont get ratelimited
 for (var x = 0; x < 10000; x+=1) { // change '100' to howmany messages u wanna send
@@ -45,15 +20,7 @@ for (var x = 0; x < 10000; x+=1) { // change '100' to howmany messages u wanna s
     request(options, function(error, body){
       if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
     })
-    request(options2, function(error, body){
-      if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
-    })
-    request(options3, function(error, body){
-      if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
-    })
-    request(options4, function(error, body){
-      if (error) console.log("Webhook is invalid"); // if we cant connect webhook = invalid
-    })
+   
   }, interval * x, x);
 }
 
